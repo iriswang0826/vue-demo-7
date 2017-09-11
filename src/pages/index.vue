b5a8ef55fae942a424948ea97573fd25852f61d7
Iris Wang <iriswang1224@163.com>
2017-09-11 16:51:55 +0800

    轮播图组件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,6 @@
           <div v-if="!product.last" class="hr"></div>
         </template>
 
-
       </div>
       <div class="index-left-block lastest-news">
         <h2>最新消息</h2>
@@ -44,7 +43,11 @@
 </template>
 
 <script>
+  import slideShow from '../components/slideshow'
   export default {
+    components: {
+      slideShow
+    },
     created: function () {
       this.$http.get('api/getNewsList')
       .then((res) => {
@@ -53,8 +56,34 @@
         console.log(err)
       })
     },
+    methods: {
+
+    },
     data () {
       return {
+        invTime: 2000,
+        slides: [
+          {
+            src: require('../assets/slideShow/pic1.jpg'),
+            title: 'xxx1',
+            href: 'detail/analysis'
+          },
+          {
+            src: require('../assets/slideShow/pic2.jpg'),
+            title: 'xxx2',
+            href: 'detail/count'
+          },
+          {
+            src: require('../assets/slideShow/pic3.jpg'),
+            title: 'xxx3',
+            href: 'http://xxx.xxx.com'
+          },
+          {
+            src: require('../assets/slideShow/pic4.jpg'),
+            title: 'xxx4',
+            href: 'detail/forecast'
+          }
+        ],
         boardList: [
           {
             title: '开放产品',
